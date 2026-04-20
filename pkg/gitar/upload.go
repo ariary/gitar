a294ca2bb0ed7cf413ccb658002e31bb2aa5f549
ariary <ariary9.2@hotmail.fr>
2026-04-20 16:33:56 +0200

    feat: add UploadZipDirectory handler for Windows PowerShell pushr

--- pkg/gitar/upload.go
+++ pkg/gitar/upload.go
@@ -1,13 +1,16 @@
 package gitar
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/ariary/gitar/pkg/config"
 	"github.com/ariary/gitar/pkg/utils"
 	"github.com/ariary/go-utils/pkg/check"
 	"github.com/ariary/go-utils/pkg/color"
@@ -73,4 +76,87 @@ func UntarDirectory(upDir string, w http.ResponseWriter, r *http.Request) {
 	check.Check(err, "Error writing to file")
 	utils.Untar(upFilename, filename)
 	check.Check(os.Remove(upFilename), "Error while remove directory tar")
+}
+
+// UploadZipHandler wraps UploadZipDirectory as an http.HandlerFunc
+func UploadZipHandler(cfg *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		remote := "(" + r.RemoteAddr + ")"
+		fmt.Print(color.Teal(remote), " ")
+		switch r.Method {
+		case "GET":
+			fmt.Println("Get request")
+			http.Error(w, "GET Bad request - Only POST accepted!", 400)
+		case "POST":
+			UploadZipDirectory(cfg.UploadDir, w, r)
+		}
+	}
+}
+
+// UploadZipDirectory receives a .zip archive and extracts it into upDir.
+// Used by the Windows PowerShell pushr alias.
+func UploadZipDirectory(upDir string, w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+
+	file, handler, err := r.FormFile("file")
+	check.Check(err, "Error Retrieving the File")
+	defer file.Close()
+
+	dirName := strings.TrimSuffix(filepath.Base(handler.Filename), ".zip")
+	fmt.Printf("Upload Directory (zip): %+v\n", color.Bold(dirName))
+
+	// Write zip to a temp file so zip.OpenReader can seek it
+	tmpFile, err := os.CreateTemp("", "gitar-*.zip")
+	check.Check(err, "Error creating temp file for zip")
+	tmpPath := tmpFile.Name()
+	defer os.Remove(tmpPath)
+
+	_, err = io.Copy(tmpFile, file)
+	check.Check(err, "Error writing zip to temp file")
+	tmpFile.Close()
+
+	destDir := filepath.Join(upDir, dirName)
+	err = unzipDir(tmpPath, destDir)
+	check.Check(err, "Error extracting zip archive")
+}
+
+// unzipDir extracts src zip archive into dest directory.
+// Returns an error if any entry would escape dest (path traversal guard).
+func unzipDir(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	dest = filepath.Clean(dest)
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+		if !strings.HasPrefix(fpath, dest+string(os.PathSeparator)) {
+			return fmt.Errorf("zip entry %q escapes destination directory", f.Name)
+		}
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, f.Mode())
+			continue
+		}
+		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return err
+		}
+		out, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+		rc, err := f.Open()
+		if err != nil {
+			out.Close()
+			return err
+		}
+		_, err = io.Copy(out, rc)
+		out.Close()
+		rc.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
